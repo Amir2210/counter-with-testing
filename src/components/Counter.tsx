1a7aa26bae95882f64027018cba2e84668233d1f
Amir Yankolovich <amiryankolovich2000@gmail.com>
2025-03-18 16:51:23 +0200

    Create 2 buttons + and -

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,10 +4,21 @@ interface CounterProps {
 }
 function Counter({ initValue }: CounterProps) {
   const [count, setCount] = useState<number>(initValue)
+
+  function onIncreaseCount() {
+    setCount((prevCount) => prevCount + 1)
+  }
+  function onDecreaseCount() {
+    setCount((prevCount) => prevCount - 1)
+  }
   return (
     <div>
       <h1>Counter With Tests (jest)</h1>
       <h2 data-testid="count">{count}</h2>
+      <div className='buttonContainer'>
+        <button onClick={onIncreaseCount}>+</button>
+        <button onClick={onDecreaseCount}>-</button>
+      </div>
     </div>
   )
 }
