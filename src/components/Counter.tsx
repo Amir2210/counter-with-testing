d1f2354c1a7ba4120b505aaee146704ef3f2a4bf
Amir Yankolovich <amiryankolovich2000@gmail.com>
2025-03-18 12:41:44 +0200

    Add state to counter cmp

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,12 +1,13 @@
-
+import { useState } from 'react'
 interface CounterProps {
   initValue: number
 }
 function Counter({ initValue }: CounterProps) {
+  const [count, setCount] = useState<number>(initValue)
   return (
     <div>
       <h1>Counter With Tests (jest)</h1>
-      <h2>{initValue}</h2>
+      <h2>{count}</h2>
     </div>
   )
 }
